4299cf371156d2ce332c6e7c7810b5fe2c3e4ef4
Youngjun Choi <juni2004c@gmail.com>
2024-02-04 21:43:09 +0900

    _와 namedimport 리팩토링을 위한 스크립트 1차 구현

--- index.ts
+++ index.ts
@@ -1,54 +1,18 @@
 import {ImportDeclaration, ImportSpecifier, Project, SourceFile} from 'ts-morph';
 import path from "path";
 
-function refactorLodashImports() {
-    const projectPath= '../BundleSizeTest';
-    const project = new Project({
-        tsConfigFilePath:  path.join(projectPath, 'tsconfig.json'),
-    });
-
-    const sourceFiles = project.getSourceFiles();
-
-    sourceFiles.forEach(sourceFile => {
-        const lodashImports = sourceFile.getImportDeclarations()
-            .filter(importDeclaration => importDeclaration.getModuleSpecifierValue() === 'lodash');
-
-        lodashImports.forEach(importDeclaration => {
-            const defaultImport = importDeclaration.getDefaultImport();
-            const namedImports = importDeclaration.getNamedImports();
-
-            if (defaultImport && defaultImport.getText() === '_') {
-                // Handle default imports (e.g., import _ from 'lodash')
-                handleDefaultImport(importDeclaration, sourceFile);
-            } else if (namedImports.length > 0) {
-                // Handle named imports (e.g., import { filter } from 'lodash')
-                handleNamedImports(importDeclaration, namedImports);
-            }
-        });
-    });
-
-    project.saveSync()
-}
-
-function findLodashUsages(importDeclaration: ImportDeclaration) {
-    const usages:string[] = [];
-    const lodashIdentifier = importDeclaration.getDefaultImport();
-
-    if (lodashIdentifier) {
-        const references = lodashIdentifier.findReferencesAsNodes();
-        references.forEach(reference => {
-            const parent = reference.getParent();
-
-            if (parent && parent.getKindName() === 'PropertyAccessExpression') {
-                usages.push(parent.getText());
-            }
-        });
-    }
+function findLodashUsages(importDeclaration: ImportDeclaration, sourceFile: SourceFile) {
+    const usages: string[] = [];
+    sourceFile.forEachDescendant(node => {
+        if (node.getKindName() === 'PropertyAccessExpression' && node.getText().startsWith('_.') && !usages.includes(node.getText())) {
+            usages.push(node.getText().replace('_.', ''));
+        }
+    })
 
     return usages;
 }
 
-function replaceLodashUsages(sourceFile :SourceFile, usages:string[]) {
+function replaceLodashUsages(sourceFile: SourceFile, usages: string[]) {
     usages.forEach(usage => {
         sourceFile.forEachDescendant(node => {
             if (node.getKindName() === 'PropertyAccessExpression' && node.getText() === `_.${usage}`) {
@@ -58,10 +22,9 @@ function replaceLodashUsages(sourceFile :SourceFile, usages:string[]) {
     });
 }
 
-function handleDefaultImport(importDeclaration: ImportDeclaration, sourceFile:SourceFile) {
+function handleDefaultImport(importDeclaration: ImportDeclaration, sourceFile: SourceFile) {
     // Find all usages of _ in the file
-    const usages = findLodashUsages(importDeclaration);
-
+    const usages = findLodashUsages(importDeclaration, sourceFile);
     // Create named imports for each unique usage
     const uniqueUsages = Array.from(new Set(usages));
     uniqueUsages.forEach(usage => {
@@ -89,6 +52,35 @@ function handleNamedImports(importDeclaration: ImportDeclaration, namedImports: 
 
     // Remove the old lodash named import declaration
     importDeclaration.remove();
+}
+
+
+function refactorLodashImports() {
+    const projectPath = process.argv[2]
+    const project = new Project({
+        tsConfigFilePath: path.join(projectPath, 'tsconfig.json'),
+    });
+
+    const sourceFiles = project.getSourceFiles();
+
+    sourceFiles.forEach(sourceFile => {
+        const lodashImports = sourceFile.getImportDeclarations()
+            .filter(importDeclaration => importDeclaration.getModuleSpecifierValue() === 'lodash');
+
+        lodashImports.forEach(importDeclaration => {
+            const defaultImport = importDeclaration.getDefaultImport();
+            const namedImports = importDeclaration.getNamedImports();
+            if (defaultImport && defaultImport.getText() === '_') {
+                // Handle default imports (e.g., import _ from 'lodash')
+                handleDefaultImport(importDeclaration, sourceFile);
+            } else if (namedImports.length > 0) {
+                // Handle named imports (e.g., import { filter } from 'lodash')
+                handleNamedImports(importDeclaration, namedImports);
+            }
+        });
+    });
+
+    project.saveSync()
 }
 
 // Run the script
