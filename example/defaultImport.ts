18705bb3d58902a8df88aaa3fae8d90218763b2c
Youngjun Choi <juni2004c@gmail.com>
2024-02-04 21:41:21 +0900

    default 예시 수정

--- example/defaultImport.ts
+++ example/defaultImport.ts
@@ -1,6 +1,13 @@
-import _ from 'lodash';
-const ver = _.VERSION;
-console.log(ver);
+import _ from "lodash";
 
+type user = {
+    user: string;
+    age: number;
+    active: boolean;
+}
+var users: user[] = [
+    {'user': 'barney', 'age': 36, 'active': true},
+    {'user': 'fred', 'age': 40, 'active': false}
+];
 
-
+_.filter(users, {'age': 36, 'active': true});
